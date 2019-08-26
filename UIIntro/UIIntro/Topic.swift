af611d4144c7effc40f92979d3125c7d2fbb41b4
james luo <luojames52@gmail.com>
2019-08-26 13:09:43 -0700

    tableView

---
+++ UIIntro/UIIntro/Topic.swift
@@ -0,0 +1,25 @@
+//
+//  Topic.swift
+//  UIIntro
+//
+//  Created by james luo on 8/26/19.
+//  Copyright © 2019 james luo. All rights reserved.
+//
+
+import UIKit
+
+class Topic: UITableViewCell {
+
+    @IBOutlet weak var Topic: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- UIIntro/UIIntro/ViewController.swift
+++ UIIntro/UIIntro/ViewController.swift
@@ -18,63 +18,13 @@ class ViewController: UIViewController {
     var LoadData: JSON?
     override func viewDidLoad() {
         //Signal for Async Process
-            self.myThread.enter()
-            AlamoPrepare()
-            let parse = DispatchWorkItem{
-                //
-                self.JsonParser()
-            }
-        
-            myThread.notify(queue: .main, work: parse)
-//        myThread.notify(queue: .main){
-//            self.JsonParser()
-//        }
-        print("mainThread")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func loadNew(_ sender: UIButton) {
-        self.JsonParser()
+        
     }
     
-    func AlamoPrepare(){
-        Alamofire.request("https://www.reddit.com/.json").responseSwiftyJSON{dataResponse in
-            //print(dataResponse.value as Any)
-            self.LoadData = JSON(dataResponse.value!)
-            print("done")
-            self.myThread.leave()
-        }
-        
     
-    }
-    func JsonParser(){
-        Alamofire.request("https://api.nasa.gov/planetary/apod?api_key=i4WERS5Lp8YotCxpWV7l07nsblcJ0vm69nauMGcK").responseSwiftyJSON(completionHandler: {nasaInfo in
-            print(nasaInfo)
-        }
-        
-        )
-        let url = URL(string: "https://api.tronalddump.io/random/meme")
-        //self.myImg.kf.setImage(with: url)
-        var title = ""
-        let dispatchGroup = DispatchGroup()
-        dispatchGroup.enter()
-        DispatchQueue.main.async {
-            if self.LoadData != nil{
-                let size = self.LoadData?["data"]["children"].arrayValue
-                
-                var target = size?.count ?? 0
-                target = Int.random(in: 0 ... target - 1)
-                title = self.LoadData?["data"]["children"][target]["data"]["title"].stringValue ?? "empty"
-                
-              //  print(title)
-                dispatchGroup.leave()
-        }
-            let cache = ImageCache.default
-            dispatchGroup.wait()
-            self.text.text = title
-            self.myImg.kf.setImage(with: url)
-            cache.clearMemoryCache()
-        }
-    }
 
 }
