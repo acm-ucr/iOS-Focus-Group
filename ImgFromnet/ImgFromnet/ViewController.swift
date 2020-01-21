45d0dc39ede4aef731402571a78675d61a2eeea8
james luo <luojames52@gmail.com>
2020-01-21 15:27:04 -0800

    animation

--- ImgFromnet/ImgFromnet/ViewController.swift
+++ ImgFromnet/ImgFromnet/ViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 import Kingfisher
+import Spring
 class ViewController: UIViewController {
 
-    @IBOutlet weak var myImg: UIImageView!
+   
+    @IBOutlet weak var myImg: SpringImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myImg.animation = "shake"
+        self.myImg.autostart = false
+        
         let url = URL(string: "https://i.redd.it/u0a8v7nss5c41.png")
        myImg.kf.setImage(with: url)
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func Animate(_ sender: UIButton) {
+        self.myImg.animate()
+        self.myImg.animation = "shake"
+        self.myImg.animateTo()
+        
+        
+        
+    }
+    
 
 }
 
